b1ad438d94fae7f18678e9d6ae254cfa3c23be38
El Bachir <57868366+kaelyavel@users.noreply.github.com>
2023-03-10 15:11:48 +0100

    feat : vdw type and read

---
+++ zt.f90
@@ -0,0 +1,29 @@
+program zt
+
+    
+    use vdw_type
+    implicit none
+
+    type(atomvdw) :: atom
+
+    type(atomvdw), dimension(53):: vdw_array
+
+    character(len=2):: na
+    real :: rad, radinser
+
+    na = "H "
+    rad = 2.5
+
+    call atom%init_vdw(na, rad)
+
+    print '(f4.2)',atom%radius
+    
+    call atom%read_vdw_file(vdw_array)
+
+    print '(f4.2)', vdw_array(1)%radius
+
+    call atom%get_atom_radius("H ", vdw_array, radinser)
+
+    print '(f4.2)', radinser
+
+end program zt
